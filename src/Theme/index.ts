2f2a67c05cd738a7a6f6b3223c22cc8290ad3da6
Nycollas <nycollas.duaarte@hotmail.com>
2020-11-09 12:31:27 -0300

    initial-structure

---
+++ src/Theme/index.ts
@@ -0,0 +1,10 @@
+const Theme = {
+  lighter: '#3F3F3F',
+  light: '#333333',
+  grey: '#2A2C2C',
+  dark: '#2A2C2C',
+  ruby: '#A11244',
+  white: '#F5F5F5'
+}
+
+export default Theme;
---
+++ src/screens/MusicPlayer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const MusicPlayer: React.FC = () => {
+  return(
+    <View>
+      
+    </View>
+  );
+}
+
+export default MusicPlayer;
