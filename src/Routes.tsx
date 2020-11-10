4a579c795efdea5d8b4e5b984a0bbf678841147a
Nycollas <nycollas.duaarte@hotmail.com>
2020-11-10 17:54:44 -0300

    updated-routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
+import theme from './Theme';
 import MusicPlayer from './screens/MusicPlayer';
 import SoundList from './screens/SoundList';
 
@@ -10,9 +13,30 @@ function Routes() {
 
   return(
     <NavigationContainer>
-      <Navigator>
-        <Screen name='Tocando' component={MusicPlayer} />
-        <Screen name='Músicas' component={SoundList} />
+      <Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Música') {
+              iconName = 'music'
+              return <FontAwesome5 name={iconName} size={size} color={color} />;
+            } else if (route.name === 'Biblioteca') {
+              iconName = 'folder-music';
+              return <Entypo name={iconName} size={size} color={color} />;
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: theme.ruby,
+          style: {
+            backgroundColor: theme.dark,
+            borderTopColor: theme.light
+          }
+        }}
+        >
+        <Screen name='Música' component={MusicPlayer} />
+        <Screen name='Biblioteca' component={SoundList} />
       </Navigator>
     </NavigationContainer>
   );
