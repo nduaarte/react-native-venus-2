f08b3b689faa6cde7c6bae34e222884348ebb0c1
Nycollas <nycollas.duaarte@hotmail.com>
2020-11-16 22:40:53 -0300

    update

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,6 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { ThemeContext } from 'styled-components';
+import { useDispatch } from "react-redux";
+
+import data from '../../data/index';
 
 import { 
   Container, 
@@ -21,9 +24,20 @@ interface propsTypes {
 function Track({ id, image, music, artist}: propsTypes) {
   const { main, lighter } = useContext(ThemeContext);
   const [hasSelect, setHasSelect] = useState(false);
+  const dispatch = useDispatch();
+
+  let stringConvert = parseInt(id);
+  const currentTrack = data[stringConvert -1];
+
+  function dispatchNewCurrentTrack() {
+    dispatch({ type: 'UPDATE_MUSIC_LINK', value: currentTrack.sound });
+    dispatch({ type: 'UPDATE_MUSIC_IMAGE', value: currentTrack.image });
+    dispatch({ type: 'UPDATE_MUSIC_NAME', value: currentTrack.music });
+    dispatch({ type: 'UPDATE_ARTIST_NAME', value: currentTrack.artist });
+  }
 
   return (
-    <Container key={id} activeOpacity={0.7}>
+    <Container key={id} activeOpacity={0.7} onPress={() => dispatchNewCurrentTrack()}>
       <MusicPic source={image} />
 
       <InfoContainer>
